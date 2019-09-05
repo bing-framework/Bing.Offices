01ec4d790f3b374e6c95009bab9de1607ec183aa
jian玄冰 <jianxuanhuo1@126.com>
2019-09-05 15:30:34 +0800

    feat: 新增注入服务

---
+++ src/Bing.Offices.Npoi/Extensions/Extensions.Service.cs
@@ -0,0 +1,28 @@
+﻿using Bing.Offices.Abstractions.Exports;
+using Bing.Offices.Abstractions.Imports;
+using Bing.Offices.Exports;
+using Bing.Offices.Imports;
+using Bing.Offices.Npoi.Exports;
+using Bing.Offices.Npoi.Imports;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bing.Offices.Npoi.Extensions
+{
+    /// <summary>
+    /// 服务扩展
+    /// </summary>
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// 注册Npoi操作
+        /// </summary>
+        /// <param name="services">服务集合</param>
+        public static void AddNpoi(this IServiceCollection services)
+        {
+            services.AddTransient<IExcelImportService, ExcelImportService>();
+            services.AddTransient<IExcelExportService, ExcelExportService>();
+            services.AddTransient<IExcelImportProvider, ExcelImportProvider>();
+            services.AddTransient<IExcelExportProvider, ExcelExportProvider>();
+        }
+    }
+}
