37d8edadaca95d4174abd5bb1844bef415260e9f
jian玄冰 <jianxuanhuo1@126.com>
2020-05-20 10:14:49 +0800

    chore: 发布 0.0.1

---
+++ Publish.bat
@@ -0,0 +1,33 @@
+@echo off
+
+::create nuget_pub
+if not exist nuget_pub (
+    md nuget_pub
+)
+
+::clear nuget_pub
+for /R "nuget_pub" %%s in (*) do (
+    del %%s
+)
+
+::Bing.Offices
+dotnet pack src/Bing.Offices.Abstractions -c Release -o nuget_pub
+dotnet pack src/Bing.Offices.Core -c Release -o nuget_pub
+dotnet pack src/Bing.Offices.Npoi -c Release -o nuget_pub
+
+for /R "nuget_pub" %%s in (*symbols.nupkg) do (
+    del %%s
+)
+
+echo.
+echo.
+
+set /p key=input key:
+set source=https://api.nuget.org/v3/index.json
+
+for /R "nuget_pub" %%s in (*.nupkg) do (
+    call dotnet nuget push %%s -k %key% -s %source%
+    echo.
+)
+
+pause
