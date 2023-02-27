cac328a1fc1df677b8db112145b2d6af935f52fb
jian玄冰 <jianxuanhuo1@126.com>
2023-02-27 17:47:40 +0800

    feat: 新增 导入头、导出头 特性

---
+++ src/Bing.Offices.Abstractions/Bing/Offices/Exports/ExportHeaderAttribute.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Bing.Offices.Exports;
+
+/// <summary>
+/// 导出头 特性
+/// </summary>
+[AttributeUsage(AttributeTargets.Property)]
+public class ExportHeaderAttribute : Attribute
+{
+    /// <summary>
+    /// 显示名称
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// 备注
+    /// </summary>
+    public string Comment { get; set; }
+
+    /// <summary>
+    /// 字体大小
+    /// </summary>
+    public float FontSize { get; set; } = 12;
+
+    /// <summary>
+    /// 是否加粗
+    /// </summary>
+    public bool IsBold { get; set; } = true;
+
+    /// <summary>
+    /// 格式化字符串
+    /// </summary>
+    public string Format { get; set; }
+
+    /// <summary>
+    /// 是否自适应
+    /// </summary>
+    public bool IsAutoFit { get; set; } = false;
+
+    /// <summary>
+    /// 自动居中
+    /// </summary>
+    public bool AutoCenterColumn { get; set; }
+
+    /// <summary>
+    /// 是否忽略
+    /// </summary>
+    public bool IsIgnore { get; set; }
+
+    /// <summary>
+    /// 宽度，<see cref="IsAutoFit"/> 为 <c>false</c> 时生效
+    /// </summary>
+    public int Width { get; set; } = 20;
+
+    /// <summary>
+    /// 排序
+    /// </summary>
+    public int ColumnIndex { get; set; } = 10000;
+
+    /// <summary>
+    /// 自动换行
+    /// </summary>
+    public bool WrapText { get; set; }
+
+    /// <summary>
+    /// 是否隐藏列
+    /// </summary>
+    public bool Hidden { get; set; }
+
+    /// <summary>
+    /// 是否图片数据
+    /// </summary>
+    public bool IsImage { get; set; } = false;
+}
---
+++ src/Bing.Offices.Abstractions/Bing/Offices/Imports/ImportHeaderAttribute.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Bing.Offices.Imports;
+
+/// <summary>
+/// 导入头 特性
+/// </summary>
+[AttributeUsage(AttributeTargets.Property)]
+public class ImportHeaderAttribute : Attribute
+{
+    /// <summary>
+    /// 显示名称
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// 批注
+    /// </summary>
+    public string Description { get; set; }
+
+    /// <summary>
+    /// 自动过滤空格，默认：启用
+    /// </summary>
+    public bool AutoTrim { get; set; } = true;
+
+    /// <summary>
+    /// 处理掉所有的空格，包括中间空格
+    /// </summary>
+    public bool FixAllSpace { get; set; }
+
+    /// <summary>
+    /// 格式化（仅用于模板生成）
+    /// </summary>
+    public string Format { get; set; }
+
+    /// <summary>
+    /// 列索引，如果为0则自动计算
+    /// </summary>
+    public int ColumnIndex { get; set; }
+
+    /// <summary>
+    /// 是否允许重复
+    /// </summary>
+    public bool IsAllowRepeat { get; set; } = true;
+
+    /// <summary>
+    /// 是否忽略
+    /// </summary>
+    public bool IsIgnore { get; set; }
+
+    /// <summary>
+    /// 是否启用Excel数据验证
+    /// </summary>
+    /// <remarks>
+    /// 对于Excel数据验证，仅用于生成导入模板特性中，作为限制用户对Excel模板数据的约束性
+    /// </remarks>
+    public bool IsInterValidation { get; set; }
+
+    /// <summary>
+    /// 选定单元格时，显示输入的信息
+    /// </summary>
+    /// <remarks>
+    /// 仅在 <see cref="IsInterValidation"/> 启用的情况下
+    /// </remarks>
+    public string ShowInputMessage { get; set; }
+
+    /// <summary>
+    /// 是否图片数据
+    /// </summary>
+    public bool IsImage { get; set; } = false;
+}
